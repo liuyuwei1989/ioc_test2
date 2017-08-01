02ecd1639c24f4637da5a634926e646de2dc3246
liuyuwei1989 <liuyuwei1989@yeah.net>
2017-08-01 11:58:00 +0800

    1

--- src/com/lyw/action/UserAction.java
+++ src/com/lyw/action/UserAction.java
@@ -1,5 +1,6 @@
 package com.lyw.action;
 
+import java.io.File;
 import java.util.List;
 
 import org.springframework.context.ApplicationContext;
@@ -16,9 +17,20 @@ public class UserAction extends ActionSupport implements ModelDriven<Employee> {
 
 	
 	private Employee employee;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Employee employee = new Employee();
 	
 	private List<Depart> departList;
 	
+	private File image;
+	
+    private String imageFileName; //文件名称
+    
+    private String imageContentType; //文件类型
 	/**
 	 * 用于注册的方法
 	 * @return
@@ -52,8 +64,7 @@ public class UserAction extends ActionSupport implements ModelDriven<Employee> {
 	
 	@Override
 	public Employee getModel() {
-		// TODO Auto-generated method stub
-		return null;
+		return employee;
 	}
 
 	public Employee getEmployee() {
@@ -64,18 +75,54 @@ public class UserAction extends ActionSupport implements ModelDriven<Employee> {
 		this.employee = employee;
 	}
 
-
-
-
 	public List<Depart> getDepartList() {
 		return departList;
 	}
 
-
-
-
 	public void setDepartList(List<Depart> departList) {
 		this.departList = departList;
+	}
+
+	/**
+	 * @return the image
+	 */
+	public File getImage() {
+		return image;
+	}
+
+	/**
+	 * @param image the image to set
+	 */
+	public void setImage(File image) {
+		this.image = image;
+	}
+
+	/**
+	 * @return the imageFileName
+	 */
+	public String getImageFileName() {
+		return imageFileName;
+	}
+
+	/**
+	 * @param imageFileName the imageFileName to set
+	 */
+	public void setImageFileName(String imageFileName) {
+		this.imageFileName = imageFileName;
+	}
+
+	/**
+	 * @return the imageContentType
+	 */
+	public String getImageContentType() {
+		return imageContentType;
+	}
+
+	/**
+	 * @param imageContentType the imageContentType to set
+	 */
+	public void setImageContentType(String imageContentType) {
+		this.imageContentType = imageContentType;
 	}
 
 	
